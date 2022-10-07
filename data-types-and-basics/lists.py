9e5cca56d3dfd1ca10293d4adb417e538713c88e
acidburn <cc.acidburn@gmail.com>
2022-10-07 09:31:12 +0200

    Practiced various list methods

--- data-types-and-basics/lists.py
+++ data-types-and-basics/lists.py
@@ -30,3 +30,77 @@ new_cart = shopping_cart[:]
 new_cart[0] = "shower gel"
 
 print(f"new: {new_cart}", f"old: {shopping_cart}")
+
+# Matrix
+# A way to describe multi-dimensional lists
+
+matrix = [
+    [1, 2, 3],
+    [2, 4, 6],
+    [7, 8, 9]
+]
+
+print(matrix[0][1])
+
+# List methods
+
+basket = ["apple", "banana", "milka", "icecream"]
+
+# Adding to list
+
+# Adds at end of list
+basket.append("cookies")
+
+# Adds to specific place in list
+basket.insert(0, "macbook")
+
+# Extends the list with another list
+basket.extend(["pear", "pineapple"])
+
+
+# Removing from list 
+
+# Removes item from specific index, defaults to last, also returns the removed value
+basket.pop()
+
+# Removes specific value 
+basket.remove("apple")
+
+# Completely clears the list
+# basket.clear()
+
+# Finds index of specific value
+basket.index("banana") 
+
+# Shows how many times a value occurs in a list
+basket.count("milka")
+
+# Sorts the list in asc or desc order, doesn't return anything, modifies original
+basket.sort()
+
+# Returns a new sorted list
+new_basket = sorted(basket)
+
+# Copies the list
+basket.copy()
+
+# Reverses the list order 
+basket.reverse()
+
+print(new_basket)
+print(basket)
+
+new_list = list(range(101))
+
+print(new_list)
+
+# Combining a list into a string
+sentence = ' '.join(['hi', 'my', 'name', 'is', 'bear'])
+
+print(sentence)
+
+# List unpacking
+
+a,b,c, *other, d = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+print(a, b, c, other, d)
--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-# Matrix
+# List unpacking
+
+a,b,c, *other, d = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+print(a, b, c, other, d)
