42b605b23584a1ec9d15fdec3e4f9b980b2e495a
acidburn <cc.acidburn@gmail.com>
2022-10-30 21:05:45 +0100

    Completed dictionary exercise

--- data-types-and-basics/dictionary.py
+++ data-types-and-basics/dictionary.py
@@ -16,3 +16,29 @@ example2 = {
     None: False
 }
 
+# I can assign a second value to get, which will be the default value if
+# the first value doesn't exist in the dictionary as a key
+example2.get("age", 12)
+
+# Alternative to create a dictionary
+example3 = dict(name='john')
+print(example3.get("name"))
+print("name" in example3)
+
+# Only checks the keys , can also be used with values
+print("a" in example.keys())
+
+# I can also use dict.items() which can return a tuple
+
+# dict.clear() - clears the whole dictionary
+
+# dict.copy() - copies the whole dictionary 
+
+# dict.pop() - pops off a value and returns it
+new_example = example.pop("a")
+print(new_example) # returns the popped off item
+print(example) # prints without the popped item
+# dict.popitem() - removes a random item, practically useless
+
+# dict.update() - updates an existing key's value, if the key is non-existent it will create a new key with the value
+example.update({'age': 55})
